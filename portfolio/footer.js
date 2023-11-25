4e9bcf6e12a3318aaf815e0a6122b191538b8c5f
Victor Soto <135081103+FixedBits@users.noreply.github.com>
2023-11-25 10:57:43 -0700

    Personal Portfolio
    Initial project files

---
+++ portfolio/footer.js
@@ -0,0 +1,22 @@
+class Footer extends HTMLElement
+{
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div id="footer">
+                <p>
+                    Copyright © 2023 <a href="https://www.bellevue.edu/">Bellevue University</a> | 
+                    <a href="https://github.com/FixedBits">GitHub</a> | 
+                    <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Web Development Degree</a> | 
+                    <a href="https://www.youtube.com/">YouTube Channel</a> | 
+                    <a href="https://github.com/buwebdev/web-330">Course GitHub</a>
+                </p>
+            </div>
+        `;
+    }
+}
+
+customElements.define("footer-component", Footer);
